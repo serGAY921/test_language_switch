c9ae30c5d3893553026520bee366164ab7b1fc98
serGAY921 <firnis8@gmail.com>
2020-06-06 18:39:42 +0700

    Написал тест, проверяющий существование кнопки

---
+++ test_items.py
@@ -0,0 +1,6 @@
+url = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+
+
+def test_existence_button(browser):
+    browser.get(link)
+    assert browser.find_element_by_css_selector(".btn-add-to-basket"), "Button not found"
