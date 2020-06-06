0bb4f9aeee74fb29e874c831df605cb5b0446bfc
serGAY921 <firnis8@gmail.com>
2020-06-06 18:42:50 +0700

    Исправил косяк с передаваемой ссылкой

--- test_items.py
+++ test_items.py
@@ -1,4 +1,4 @@
-url = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 
 
 def test_existence_button(browser):
