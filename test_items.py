791f7595f5e1cad2435321ef526fcabd8a9b5040
serGAY921 <firnis8@gmail.com>
2020-06-06 19:02:44 +0700

    Еще раз изменил ссылку, теперь работает корректно

--- test_items.py
+++ test_items.py
@@ -1,6 +1,8 @@
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_existence_button(browser):
     browser.get(link)
     assert browser.find_element_by_css_selector(".btn-add-to-basket"), "Button not found"
+    time.sleep(5)
